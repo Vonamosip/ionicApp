fb10854c8f7714e305418a07d0cf14a939139a95
Vonamosip <sashadrozdov11@mail.ru>
2025-01-29 11:36:05 +0300

    feature: added saving photos functionality

--- photo-gallery/src/app/tab2/tab2.page.ts
+++ photo-gallery/src/app/tab2/tab2.page.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { IonFab, IonFabButton, IonIcon } from '@ionic/angular/standalone';
 import { PhotoService } from '../services/photo.service';
-
+import { IonCol, IonGrid, IonRow } from '@ionic/angular/standalone';
+import { IonImg } from '@ionic/angular/standalone';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon, IonCol, IonGrid, IonRow,IonImg]
 })
 export class Tab2Page {
 
